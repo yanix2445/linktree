fff963ff832ae2763d3d7b7b49978de58422188a
Yanis Harrat <yanis.amine.harrat@gmail.com>
2026-02-10 09:30:56 +0100

    chore(config): configuration des domaines d'images autorisés
    Ajout des domaines distants dans next.config.ts pour next/image :
    GitHub avatars, GitHub OG images, opengraph.githubassets.com
    et repository-images.githubusercontent.com pour les repos épinglés.
    
    N/A

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "github.com" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "picsum.photos" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "fastly.picsum.photos" },
+      { protocol: "https", hostname: "img.youtube.com" },
+      { protocol: "https", hostname: "i.ytimg.com" },
+      { protocol: "https", hostname: "opengraph.githubassets.com" },
+      { protocol: "https", hostname: "repository-images.githubusercontent.com" },
+    ],
+  },
 };
 
 export default nextConfig;
